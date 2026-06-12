cb5a751bd86ab6b40ac64855a86a9499e169d649
Mustapha <mustaphabusari2005@gmail.com>
2026-06-12 09:33:28 +0100

    vehicle booking flows

--- src/routes/transportRoute.js
+++ src/routes/transportRoute.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { verifyUser } from "../middlewares/auth.js";
-import { bookSeat, cancelBooking } from "../controllers/transport.js";
+import {
+  createBooking,
+  getMyBooking,
+  cancelBooking,
+} from "../controllers/transport.js";
 
 const router = express.Router();
 
-router.post("/book", verifyUser, bookSeat);
-
-router.post("/cancel", verifyUser, cancelBooking);
+router.post("/book", verifyUser, createBooking);
+router.get("/my-booking", verifyUser, getMyBooking);
+router.put("/cancel", verifyUser, cancelBooking);
 
 export default router;
