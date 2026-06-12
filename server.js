35624a18ec0969fe7786bf4deb5f1aa79b9397ca
muskid <mustaphabusari2005@gmail.com>
2026-06-12 17:51:36 +0100

    Update CORS origin to allow multiple hosts

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5500",
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
     credentials: true,
   }),
 );
