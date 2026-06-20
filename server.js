57bf72ff67dd8182f224c2e48bf5b3448b9fd3d4
muskid <mustaphabusari2005@gmail.com>
2026-06-20 02:50:38 +0100

    Add Vercel app to CORS allowed origins

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500', 'https://transit-key.vercel.app'],
     credentials: true,
   }),
 );
